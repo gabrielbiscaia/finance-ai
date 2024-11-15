44aee4e1bcf517ad9826111e3a214952a62a50e0
gabrielbiscaia <biscadev@gmail.com>
2024-11-15 20:09:35 -0300

    chore: add git hook setup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,11 @@ const Home = async () => {
   if (!userId) {
     redirect("/login");
   }
-  return <UserButton showName />;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
